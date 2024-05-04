da920d9b5c41afe52374ab045916653d825195a2
Agah <agahgames260@gmail.com>
2024-05-05 02:40:57 +0300

    zombie spawners midway commit

--- Assets/Scripts/Runtime/Managers/LaserZombieManager.cs
+++ Assets/Scripts/Runtime/Managers/LaserZombieManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using Runtime.Interfaces;
 using Runtime.Signals;
 using UnityEngine;
@@ -27,14 +26,10 @@ namespace Runtime.Managers
 
         private int currentHealth = 100;
         private int maxHealth = 100;
-        private int ZOMBIE_POOL_SIZE = 100;
-        private Queue<GameObject> laserZombieQueue = new Queue<GameObject>();
-
+       
         #endregion
 
         #endregion
-
-
 
         private bool isAttacking = false;
 
@@ -49,11 +44,6 @@ namespace Runtime.Managers
         {
             TimerSignals.Instance.OnThirtySecondsPassed += () => LevelUpZombie(1);
         }
-        private void Start()
-        {
-            CreateZombiePool();
-        }
-
         private void Update()
         {
             if (target != null && !isAttacking)
@@ -116,42 +106,8 @@ namespace Runtime.Managers
 
         private void Die()
         {
-            ReturnZombieToPool(gameObject);
+           LaserZombiePool.Instance.ReturnZombieToPool(gameObject);
         }
-
-        #region Object Pooling Methods
-
-        public void CreateZombiePool()
-        {
-            for (int i = 0; i < ZOMBIE_POOL_SIZE; i++)
-            {
-                GameObject zombie = Instantiate(gameObject, Vector3.zero, Quaternion.identity);
-                zombie.SetActive(false);
-                laserZombieQueue.Enqueue(zombie);
-            }
-        }
-
-        public GameObject GetZombieFromPool()
-        {
-            if (laserZombieQueue.Count > 0)
-            {
-                GameObject zombie = laserZombieQueue.Dequeue();
-                zombie.SetActive(true);
-                return zombie;
-            }
-
-            return null;
-        }
-
-        public void ReturnZombieToPool(GameObject zombie)
-        {
-            zombie.SetActive(false);
-            laserZombieQueue.Enqueue(zombie);
-        }
-
-      
-
-        #endregion
-
+    
     }
 }
--- Assets/Scripts/Runtime/Managers/TimerManager.cs
+++ Assets/Scripts/Runtime/Managers/TimerManager.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+using Runtime.Managers;
 using Runtime.Signals;
 using System;
 using TMPro;
@@ -38,7 +40,7 @@ namespace Runtime.Managers
         }
         private void FireEvents()
         {
-            if ((int)elapsedTime % 30 == 0 && elapsedTime %60 != 0 && elapsedTime % 300 != 0)
+            if ((int)elapsedTime % 30 == 0 && elapsedTime % 60 != 0 && elapsedTime % 300 != 0)
             {
                 TimerSignals.Instance.OnThirtySecondsPassed();
             }
@@ -64,17 +66,73 @@ namespace Runtime.Managers
 
 public class ZombieSpawner : MonoBehaviour
 {
-    //Will spawn zombies every 5 seconds
-    private void Update()
+
+    public bool canSpawnZombies = false;
+    public bool canSpawnSpitters = false;
+    public bool canSpawnLasers = false;
+
+    private void Start()
     {
-        if ((int)Time.time % 5 == 0)
+        SpawnZombies();
+        SpawnSpitters();
+        SpawnLasers();
+        
+    }
+    private async void SpawnZombies()
+    {
+        while (canSpawnZombies)
         {
-            SpawnZombie();
+            GameObject zombie = ZombiePool.Instance.GetZombieFromPool();
+            if (zombie != null)
+            {
+                zombie.transform.position = transform.position;
+                zombie.transform.rotation = transform.rotation;
+                zombie.SetActive(true);
+            }
+            await UniTask.WaitForSeconds(1.5f);
+            if(ZombiePool.Instance.CurrentZombieCount >= 99)
+            {
+                canSpawnZombies = false;
+                break;
+            }
         }
     }
-
-    private void SpawnZombie()
+    private async void SpawnSpitters()
     {
-
+        while (canSpawnSpitters)
+        {
+            GameObject zombie = SpittingZombiePool.Instance.GetZombieFromPool();
+            if (zombie != null)
+            {
+                zombie.transform.position = transform.position;
+                zombie.transform.rotation = transform.rotation;
+                zombie.SetActive(true);
+            }
+            await UniTask.WaitForSeconds(1.5f);
+            if (SpittingZombiePool.Instance.CurrentZombieCount >= 25)
+            {
+                canSpawnZombies = false;
+                break;
+            }
+        }
+    }
+    private async void SpawnLasers()
+    {
+        while (canSpawnLasers)
+        {
+            GameObject laser = LaserZombiePool.Instance.GetZombieFromPool();
+            if (laser != null)
+            {
+                laser.transform.position = transform.position;
+                laser.transform.rotation = transform.rotation;
+                laser.SetActive(true);
+            }
+            await UniTask.WaitForSeconds(1.5f);
+            if (LaserZombiePool.Instance.CurrentZombieCount >= 10)
+            {
+                canSpawnZombies = false;
+                break;
+            }
+        }
     }
 }
