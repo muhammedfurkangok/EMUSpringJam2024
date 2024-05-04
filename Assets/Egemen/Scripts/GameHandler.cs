c1fd5ea8682f2d15ee56d723d55baeebcee90a47
Egemen Azay <egemenazay39@gmail.com>
2024-05-04 03:49:36 +0300

    healthbar complito

--- Assets/Egemen/Scripts/GameHandler.cs
+++ Assets/Egemen/Scripts/GameHandler.cs
@@ -1,13 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameHandler : MonoBehaviour
 {
-    [SerializeField] private HealthBar healthBar;
-    void Start()
-    {
-        healthBar.SetSize(.4f);
-    }
     
 }
--- Assets/Egemen/Scripts/HealthBar.cs
+++ Assets/Egemen/Scripts/HealthBar.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     private Transform bar;
-    void Start()
+    void Awake()
     {
         bar = transform.Find("Bar");
+        Debug.Log(bar);
     }
 
     public void SetSize(float SizeNormalized)
     {
+        if (SizeNormalized > 1f)
+        {
+            Debug.LogWarning("1den büyük yollama amk");
+        }
         bar.localScale = new Vector3(SizeNormalized, 1f);
     }
 }
